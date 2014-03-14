26b1111339b678c6f9a699307a1bbd725978d3d5
daamca <daamca@gmail.com>
2014-03-14 13:48:46 +0100

    primera

---
+++ gameZ/src/com/me/mygdxgame/GameZcontroller.java
@@ -0,0 +1,16 @@
+package com.me.mygdxgame;
+
+public class GameZcontroller {
+
+	public GameZcontroller() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void init(){
+		
+	}
+	
+	public void update(float DeltaTime){
+		
+	}
+}
